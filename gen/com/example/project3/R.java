cbc422c06516b0010a6785fe4d8860bf8bd84e82
Alex Hoffman <alexhoffman617@gmail.com>
2013-02-26 14:38:11 -0500

    added date scrolling to graphing page"

--- gen/com/example/project3/R.java
+++ gen/com/example/project3/R.java
@@ -14,11 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int EditText01=0x7f070002;
-        public static final int LinearLayout01=0x7f070001;
-        public static final int ListView01=0x7f070003;
+        public static final int EditText01=0x7f070004;
+        public static final int LinearLayout01=0x7f070003;
+        public static final int ListView01=0x7f070005;
         public static final int button=0x7f070000;
-        public static final int menu_settings=0x7f070004;
+        public static final int graphDateEnd=0x7f070001;
+        public static final int graphDateStart=0x7f070002;
+        public static final int menu_settings=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
