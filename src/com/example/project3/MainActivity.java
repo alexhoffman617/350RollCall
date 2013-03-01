28cc74a606229dfb3346afc70455ab093ecb6216
Alex Hoffman <alexhoffman617@gmail.com>
2013-02-28 19:39:09 -0500

    added parse sdk

--- src/com/example/project3/MainActivity.java
+++ src/com/example/project3/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.project3;
 
 import android.os.Bundle;
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.PushService;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
@@ -13,12 +17,19 @@ public class MainActivity extends Activity implements OnClickListener{
 
 	Button btn;
 	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Parse.initialize(this, "cuoXWbqvBKs8SUrhnyKdyNWiMPZxuDBZ31ehltVI", "tl8VMcFHu7u3haym9KSbRKEP61MmxPDvmL06dxeo");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		btn=(Button)findViewById(R.id.button);
 		btn.setOnClickListener((OnClickListener) this);
+		
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
 	}
 
 	@Override
